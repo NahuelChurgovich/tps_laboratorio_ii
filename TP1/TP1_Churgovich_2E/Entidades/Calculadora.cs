5077575cc4fc7fddb77defd3143dac105d8e0867
NahuelChurgovich <nchurgovich@gmail.com>
2021-09-10 20:09:12 -0300

    Creo la clase Calculadora y Operando en Entidades. Hago los 2 métodos de Calculadora

---
+++ TP1/TP1_Churgovich_2E/Entidades/Calculadora.cs
@@ -0,0 +1,27 @@
+﻿namespace Entidades
+{
+    #region Métodos
+    private static char ValidarOperador(char operador)
+    {
+        if (operador == '+' || operador == '-' || operador == '/' || operador == '*')
+        {
+            return operador;
+        }
+        return '+';
+    }
+    public static double Operar(Operando num1, Operando num2, char operador)
+    {
+        switch (ValidarOperador(operador))
+        {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '/':
+                return num1 / num2;
+            default:
+                return num1 * num2;
+        }
+    }
+    #endregion
+}
