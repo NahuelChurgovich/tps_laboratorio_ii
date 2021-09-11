be71d8ef1404393e1d81cda4682272007e2f0e81
NahuelChurgovich <nchurgovich@gmail.com>
2021-09-10 21:15:21 -0300

    Atributos, Constructores, Propiedades y Metodos de clase Operando

--- TP1/TP1_Churgovich_2E/Entidades/Operando.cs
+++ TP1/TP1_Churgovich_2E/Entidades/Operando.cs
@@ -1,13 +1,168 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Entidades
 {
     public class Operando
     {
+        #region Atributos
+
+        private double numero;
+
+        #endregion
+
+        #region Constructor
+
+        public Operando() : this(0)
+        {
+        }
+
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Operando(string strNumero)
+        {
+            this.Numero = strNumero;
+        }
+        #endregion
+
+        #region Propiedades
+
+        private string Numero
+        {
+            set
+            {
+                this.numero = ValidarOperando(value);
+            }
+        }
+        #endregion
+
+        #region Métodos
+
+        private double ValidarOperando(string strNumero)
+        {
+            if (strNumero != null && double.TryParse(strNumero, out double auxNum))
+            {
+                return auxNum;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private bool EsBinario(string binario)
+        {
+            if (binario != null)
+            {
+                foreach (char digito in binario)
+                {
+                    if (digito != '0' || digito != '1')
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            double decima = 0;
+            if (EsBinario(binario))
+            {
+                char[] arrayBinario = binario.ToCharArray();
+                Array.Reverse(arrayBinario);
+                for (int i = 0; i < arrayBinario.Length; i++)
+                {
+                    if (arrayBinario[i] == '1')
+                    {
+                        decima += (double)Math.Pow(2, i);
+                    }
+                }
+                return decima.ToString();
+            }
+            else
+            {
+                return "Valor inválido";
+            }
+        }
+
+        public string DecimalBinario(double numero)
+        {
+            const int dos = 2;
+            double decima = numero;
+            string resto = "";
+            char[] arrayAux = { ' ' };
+            int i = 0;
+
+            if (decima >= 0)
+            {
+                while (decima >= 1)
+                {
+                    decima = Math.Truncate(decima);
+                    resto += (decima % dos).ToString();
+                    decima /= dos;
+                    i++;
+                }
+                arrayAux = resto.ToCharArray();
+                Array.Reverse(arrayAux);
+                return new string(arrayAux);
+            }
+            else
+            {
+                return "Valor inválido";
+            }
+        }
+
+        public string DecimalBinario(string numero)
+        {
+            if (double.TryParse(numero, out double aux))
+            {
+                return DecimalBinario(aux);
+            }
+            else
+            {
+                return "Valor inválido";
+            }
+        }
+        #endregion
+
+
+        #region Sobrecargas
+        ////REVISAR EL MODIFICADOR DE EXPLICIT E IMPLICIT
+        //public static double operator +(Operando n1, Operando n2)
+        //{
+        //    return n1.numero + n2.numero;
+        //}
+
+        ////REVISAR EL MODIFICADOR DE EXPLICIT E IMPLICIT
+        //public static double operator -(Operando n1, Operando n2)
+        //{
+        //    return n1.numero - n2.numero;
+        //}
+
+        ////REVISAR EL MODIFICADOR DE EXPLICIT E IMPLICIT
+        //public static double operator /(Operando n1, Operando n2)
+        //{
+        //    if (n2.numero == 0)
+        //    {
+        //        return double.MinValue;
+        //    }
+        //    else
+        //    {
+        //        return n1.numero / n2.numero;
+        //    }
+        //}
+
+        ////REVISAR EL MODIFICADOR DE EXPLICIT E IMPLICIT
+        //public static double operator *(Operando n1, Operando n2)
+        //{
+        //    return n1.numero * n2.numero;
+        //}
+        #endregion
 
     }
 }
