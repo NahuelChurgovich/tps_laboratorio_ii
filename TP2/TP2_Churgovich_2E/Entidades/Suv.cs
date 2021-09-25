ffa5cb09d0af19a8ef2b6ef1a1c1efce41f46994
NahuelChurgovich <nchurgovich@gmail.com>
2021-09-24 22:52:29 -0300

    Correccion de la clase SUV

--- TP2/TP2_Churgovich_2E/Entidades/Suv.cs
+++ TP2/TP2_Churgovich_2E/Entidades/Suv.cs
@@ -1,39 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class Suv
+    public class Suv : Vehiculo
     {
-        public Suv(EMarca marca, string chasis, ConsoleColor color)
-            : base(chasis, marca, color)
-        {
-        }
+        #region Propiedades
+
         /// <summary>
         /// SUV son 'Grande'
         /// </summary>
-        protected override short Tamanio
+        protected override ETamanio Tamanio
         {
             get
             {
-                return 0;
+                return ETamanio.Grande;
             }
         }
+
+        #endregion
+
+
+        #region Constructores
+
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+          : base(marca, chasis, color)
+        {
+        }
+
+        #endregion
+
+
+        #region Métodos
 
         public override sealed string Mostrar()
         {
             StringBuilder sb = new StringBuilder();
 
             sb.AppendLine("SUV");
-            sb.AppendLine(base);
-            sb.AppendLine("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
             sb.AppendLine("");
             sb.AppendLine("---------------------");
 
-            return sb;
+            return sb.ToString();
         }
+
+        #endregion
+
+
+
+
+
     }
 }
